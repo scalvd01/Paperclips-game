2a60e6d7c33455f0a51b4d579c5d2c55d0373898
Santi <scalvd01@estudiantes.unileon.es>
2024-05-19 00:07:48 +0200

    Logo changed

--- src/components/PaperclipLogo.vue
+++ src/components/PaperclipLogo.vue
@@ -1,22 +1,26 @@
 <script setup></script>
 <template>
   <div class="">
-    <h1 class="text-3xl font-bold flex items-baseline text-gray-800 dark:text-white">
-      P<svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="h-5 stroke-2"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3 3 0 1119.5 7.372L8.552 18.32m.009-.01l-.01.01m5.699-9.941l-7.81 7.81a1.5 1.5 0 002.112 2.13"
-        />
-      </svg>
-      perclips
+    <h1 class="absolute text-3xl font-bold flex items-baseline text-gray-800 dark:text-white">
+      Papercl
+      <div class="flex flex-col items-center">
+        <span class="fixed top-0 mt-1">.</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="h-4 stroke-2 -rotate-45"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3 3 0 1119.5 7.372L8.552 18.32m.009-.01l-.01.01m5.699-9.941l-7.81 7.81a1.5 1.5 0 002.112 2.13"
+          />
+        </svg>
+      </div>
+      ps
     </h1>
   </div>
 </template>
