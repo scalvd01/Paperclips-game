db78dc0524cd964c3c43d4371eb9479f17362d6d
Santi <scalvd01@estudiantes.unileon.es>
2024-05-20 12:22:34 +0200

    Fixed makePaperclipButton spacing

--- src/components/MakePaperclipButton.vue
+++ src/components/MakePaperclipButton.vue
@@ -7,9 +7,9 @@ const game = inject('game')
   <button
     type="button"
     :disabled="game.wireLongitude <= 0"
-    class="text-xl mr-1 my-0.5 px-1.5 pb-1 rounded bg-slate-300 disabled:bg-neutral-100 disabled:text-neutral-400 enabled:hover:bg-slate-400 enabled:active:scale-95 dark:enabled:hover:bg-slate-600 dark:enabled:bg-slate-700 dark:disabled:bg-slate-700/40 dark:disabled:text-slate-500"
+    class="px-2 py-1 text-xl rounded bg-slate-300 disabled:bg-neutral-100 disabled:text-neutral-400 enabled:hover:bg-slate-400 enabled:active:scale-95 dark:enabled:hover:bg-slate-600 dark:enabled:bg-slate-700 dark:disabled:bg-slate-700/40 dark:disabled:text-slate-500"
     @click="() => game.addPaperclips(1)"
   >
-    <span >{{ $t('makePaperclip') }}</span>
+    <span>{{ $t('makePaperclip') }}</span>
   </button>
 </template>
